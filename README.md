6dbb4e317d102c78b3f0771b95c84d2f5ea5d181
Jessie Wang <j.wang@comicrelief.com>
2017-07-29 21:00:14 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# grants-infographics
+# Grants open data hackday prototype
+> 
+
+## Preview
+
+## Resources
+
+[Grants api](https://github.com/comicrelief/grants-api-serverless)
+[Grants api doc] (http://cr-grants-api-documentation.s3-website-eu-west-1.amazonaws.com/#api-Grants-GetGrant)
+[D3.js](https://github.com/d3/d3)
+
---
+++ js/common.js
@@ -0,0 +1,72 @@
+/*
+ * 
+ */
+(($) => {
+
+  let apiURL = 'https://1kfs7evxca.execute-api.eu-west-1.amazonaws.com/beta/grants',
+      grantsIssue,
+      freqData = [],
+      grantsTotal = 0,
+      ukTotal = 0;
+
+
+  $.getJSON( apiURL, function( data ) {
+
+    grantsIssue = data.data['facets'].issue
+    grantsProjects = data.data['pagination'].total
+    ukTotal = parseInt(data.data['facets'].country_name[0].total_awarded)
+
+    // Calulate overall awarded
+    for (var i = 0; i < grantsIssue.length; i++) {
+      grantsTotal += parseInt(grantsIssue[i].total_awarded) << 0;
+    } 
+
+    $('.grant_total').append('&pound;' + parseInt(grantsTotal/1000000))
+    $('.grant_projects').append( grantsProjects);
+    $('.grant_issues').append( grantsIssue.length);
+
+  });
+
+ // TODO: open issue on github for api to provide list, 
+ //currently there isn't way to get list id dynamically
+  var issueIDs = ['131', '219', '207', '218', '220', 
+                  '222', '108', '96', '269', '266',
+                  '210', '101', '212', '268', '65',
+                  '203', '217', '265', '271', '169',
+                  '270', '8', '109', '267', '214']
+
+  for (let i = 0; i < issueIDs.length; i++) {
+      $.getJSON( apiURL + '?fields=issue_id&search=' + issueIDs[i], function () {
+      })
+      .done(function(myData) {
+
+        dashboardData(
+          i,
+          myData.data['facets'].issue[0].key, 
+          myData.data['facets'].country_name[0].count,
+          myData.data['pagination'].total - myData.data['facets'].country_name[0].count,
+          myData.data['pagination'].total
+        ); 
+      });
+  } 
+
+function dashboardData(order, name, uk, inl, total) {
+  freqData.push(
+    { Issue: name,
+      freq: { 
+        low: uk, 
+        mid: inl 
+      },
+      total: total,
+      name: order
+    }
+  );
+
+  // Temporary of dealing with aysnc issue
+  if ( freqData.length == issueIDs.length) {
+    // call d3js
+    dashboard('#dashboard',freqData);
+  }
+}  
+
+})(jQuery)
